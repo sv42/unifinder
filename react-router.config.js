bbfde078407c674c324dcc7bc529f99895adffed
Vova Stoliarchuk <vovkastoliarchuk@gmail.com>
2025-07-20 20:58:30 +0300

    Fix base URL for GitHub Pages deployment

--- react-router.config.js
+++ react-router.config.js
@@ -2,4 +2,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  basename: '/unifinder',
 }; 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: '/unifinder/',
   plugins: [tailwindcss(), reactRouter()],
 }); 
